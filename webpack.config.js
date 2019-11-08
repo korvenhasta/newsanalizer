45f8f08e75d7c6ccf51566c8abd1d9c70108fac8
Marina Kenina <avrora-lisa@yandex.ru>
2019-11-08 13:09:34 +0300

    added settings to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+//const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/index.js',
+    about: './src/about.js',
+    paper: './src/paper.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -22,19 +27,62 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        use: [
+          //(isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/i,
+        use: [
+          'file-loader?name=./images/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-        filename: 'style.[contenthash].css'
+        filename: './pages/[name].[contenthash].css'
     }),
-    new HtmlWebpackPlugin({ // настроили плагин
-        inject: false,
-        template: './src/index.html',
-        filename: 'index.html'
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/about.html',
+      filename: 'about.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/paper.html',
+      filename: 'paper.html'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
     }),
     new WebpackMd5Hash()
+    // new webpack.DefinePlugin({
+    //   'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    //  })
   ]
 };
