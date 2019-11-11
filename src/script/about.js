48ebb1d4432af2536ddab9e55b8cccdf71122f46
Marina Kenina <avrora-lisa@yandex.ru>
2019-11-11 22:31:20 +0300

    added slider

--- src/script/about.js
+++ src/script/about.js
@@ -1,3 +1,12 @@
 import "../pages/about.css";
+import Glide from '../../node_modules/@glidejs/glide/dist/glide'
+
+new Glide('.glide', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 3,
+  peek: 88,
+  focusAt: 'center'
+}).mount()
 
 //const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort3' : 'https://praktikum.tk/cohort3'
